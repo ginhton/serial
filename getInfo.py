9d425282a5cabc0cc912b5ee016bb26f4704c8e9
ginhton <ginhton@qq.com>
2019-03-21 14:16:39 +0800

    get more information about node

--- getInfo.py
+++ getInfo.py
@@ -3,15 +3,24 @@ import binascii
 import timeit
 from time import sleep
 
-def Hex(command, n):
+def Hex(command):
     hexStr = bytes.fromhex(command)
     num = t.write(hexStr);
     data = t.read(1)
     data += t.read(t.inWaiting());
     return data
 
+# command
+commands = [
+    'fe 01 02 ff', # network status
+    'fe 01 05 ff', # local network short address
+    'fe 01 06 ff', # local MAC address
+    'fe 09 10 mac_addr ff' # get short address for specific mac address
+]
+
 t = serial.Serial('/dev/ttyUSB0', 115200)
 
-data = Hex('fe 09 10 46 16 dc 1b 00 4b 12 00 ff', 3)
+# sample end node address
+# data = Hex('fe 09 10 46 16 dc 1b 00 4b 12 00 ff')
 
 print(str(binascii.b2a_hex(data)))
